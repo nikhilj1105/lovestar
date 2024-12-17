6d455ca8a08e355e295198d99da9b06511793987
Nikhil Jain <nikhil.jain2@Infoedge.com>
2024-12-17 16:50:44 +0530

    removed next folder from the build

--- .next/static/chunks/app/page-91322dbf62e8b677.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{2036:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,3572,23))},3572:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[283,173,441,517,358],()=>s(2036)),_N_E=e.O()}]);
